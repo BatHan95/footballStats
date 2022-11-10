b8a80a426c75e6a71ba8a935c426fbfa1d9aaef6
Matt Speight <matthew1270.ms@gmail.com>
2022-11-10 17:06:53 +0000

    🔧 Adjust config setup

--- rdsConnector.py
+++ rdsConnector.py
@@ -2,20 +2,15 @@ import psycopg2
 import json
 import postgresQueries
 
-ENDPOINT="database-1.cn2p5chwsam4.eu-west-2.rds.amazonaws.com"
-PORT="5432"
-USER="postgres"
-REGION="eu-west-2c"
-DBNAME="footballStats"
-PASSWORD = json.loads(open('.env', 'r').read())['RDS_PASSWORD']
+config = json.loads(open('config.json', 'r').read())
 
 def rdsConnection():
     try:
-        conn = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME, user=USER, password=PASSWORD, sslrootcert="SSLCERTIFICATE")
+        conn = psycopg2.connect(host=config["main_rds_host"], port=config["main_rds_port"], database=config["main_rds_stats_db_name"], user=config["main_rds_username"], password=config["main_rds_password"], sslrootcert="SSLCERTIFICATE")
         cur = conn.cursor()
     except Exception as e:
         print("Database connection failed due to {}".format(e)) 
-    # print('RDS connection established')
+    print('RDS connection established')
     return cur, conn
 
 def rdsInsert(df, type):
