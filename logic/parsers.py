0545ce6c863e2fb80f72218195c0bf5f80311c53
Matt Speight <matthew1270.ms@gmail.com>
2022-11-11 18:19:27 +0000

    add some formatting to the parser

--- logic/parsers.py
+++ logic/parsers.py
@@ -5,7 +5,7 @@ def parse_bets_for_email(data):
 
     for match in data["matches"]:
         for game in match:
-            output_string += match[game]["teams"]["home"] + ' vs ' + match[game]["teams"]["away"] + '<br>'
+            output_string += '<u>' + match[game]["teams"]["home"] + ' vs ' + match[game]["teams"]["away"] + '</u><br>'
             for bet in match[game]["bets"]:
                 output_string += bet + '<br>'
             output_string += '<br>'
