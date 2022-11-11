d31be30a24275038af227cf51d2d7f26a563dc8d
Matt Speight <matthew1270.ms@gmail.com>
2022-11-11 17:11:02 +0000

    adjust api response

--- handler.py
+++ handler.py
@@ -2,29 +2,38 @@ import json
 import main
 API_KEY = json.loads(open('config.json', 'r').read())["API_KEY"]
 
+
 def run(event, context):
 
     status = False
     updateText = ''
+
     try:
-        if(event['api_key'] == API_KEY):
-            try:
-                if (event['action'] == 'refresh'):
-                    # Run refresh logic
-                    main.refresh()
-                    status = True
-                elif (event['action'] == 'getBetsForTomorrow'):
-                  # Run get bets logic
-                    main.getBetsForTomorrow()
-                    status = True
-                else:
-                    raise Exception('No action provided')
-            except:
-                updateText = 'Issue with the action'
-        else:
-            raise Exception('Incorrect API Key')
+        if(event['api_key'] != API_KEY):
+            response = {
+                "statusCode": 200 if status else 403,
+                "context": 'Issue with authentication'
+            }
+
+            return response
     except:
-        updateText = 'Issue with authentication'
+            response = {
+                "statusCode": 200 if status else 403,
+                "context": 'Issue with authentication'
+            }
+
+            return response
+
+    if (event['action'] == 'refresh'):
+        # Run refresh logic
+        main.refresh()
+        status = True
+    elif (event['action'] == 'getBetsForTomorrow'):
+        # Run get bets logic
+        main.getBetsForTomorrow()
+        status = True
+    else:
+        raise Exception('No action provided')
 
     response = {
         "statusCode": 200 if status else 403,
