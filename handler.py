a1f86a48929ecd10c6aa1baaa2a756c184d38e80
Matt Speight <matthew1270.ms@gmail.com>
2022-11-27 13:48:43 +0000

    update incoming

--- handler.py
+++ handler.py
@@ -4,49 +4,61 @@ import logic.parsers as parser
 
 API_KEY = json.loads(open('config.json', 'r').read())["API_KEY"]
 
-
 def run(event, context):
 
     status = False
     updateText = ''
-
     try:
-        if(event['api_key'] != API_KEY):
-            response = {
-                "statusCode": 200 if status else 403,
-                "context": 'Issue with authentication'
-            }
+        try:
+            if(event['api_key'] != API_KEY):
+                response = {
+                    "statusCode": 200 if status else 400,
+                    "context": '403 - Issue with authentication'
+                }
+                return response
+        except:
+                response = {
+                    "statusCode": 200 if status else 400,
+                    "context": '403 - Issue with authentication'
+                }
+                return response
+        
+        try:
+            event['action']
+        except:
+            updateText = '400 - No action provided'
+            raise Exception('No action provided')
 
+        if (event['action'] == 'refresh'):
+            # Run refresh logic
+            main.refresh()
+            updateText = '200 - Refresh Successful'
+            status = True
+        elif (event['action'] == 'getBetsForTomorrow'):
+            # Run get bets logic
+            updateText = main.getBetsForTomorrow()
+            status = True
+        elif (event['action'] == 'getBetsForTomorrow_email'):
+            response_raw = main.getBetsForTomorrow()
+            response = parser.parse_bets_for_email(response_raw)
             return response
+        else:
+            updateText = f"""{event['action']} is not a valid action"""
+
     except:
-            response = {
-                "statusCode": 200 if status else 403,
-                "context": 'Issue with authentication'
-            }
-
-            return response
-
-    if (event['action'] == 'refresh'):
-        # Run refresh logic
-        main.refresh()
-        updateText = 'Refresh Successful'
-        status = True
-    elif (event['action'] == 'getBetsForTomorrow'):
-        # Run get bets logic
-        updateText = main.getBetsForTomorrow()
-        status = True
-    elif (event['action'] == 'getBetsForTomorrow_email'):
-        response_raw = main.getBetsForTomorrow()
-        response = parser.parse_bets_for_email(response_raw)
-        return response
-    else:
-        raise Exception('No action provided')
-
+        response = {
+            "statusCode": 200 if status else 400,
+            "context": updateText
+        }
+    
     response = {
-        "statusCode": 200 if status else 403,
-        "context": updateText
+            "statusCode": 200 if status else 400,
+            "context": updateText
     }
 
     return response
 
-print(run({},{}))
+print(run({
+    'api_key': API_KEY,
+    'action': 'test_email'
+},{}))
